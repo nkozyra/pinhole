a04f9ce6d7efaf884be4693beccc095cddbb93c5
Nathan Kozyra <nkozyra@gmail.com>
2016-01-13 11:15:48 -0500

    converts frames to pixelblocks

--- datagobbler.py
+++ datagobbler.py
@@ -1,29 +1,60 @@
 import argparse
 import cv2
-import numpy as nd
+import numpy as np
 import sys
-
+from tqdm import tqdm
 
 # green chromakey
-chromaKey = nd.array([0,255,0])
+chromaKey = np.array([0,255,0])
 blocks = 100
-maxFrames = 20
+maxFrames = 2
+destination = "images/"
 
+
+dataFrames = np.array((int(blocks),int(blocks)))
 def process(f):
   c = cv2.VideoCapture(f)
-  i = 0
+  frame = 0
+  totalBlocks = int(blocks)
   # if c.isOpened():
   while(True):
     t,data = c.read()
+
     height = len(data)
     width = len(data[0])
-    hBlocks = width / int(blocks)
-    vBlocks = height / int(blocks)
-    print vBlocks,hBlocks
-    for ii in range(height):
-      pass
-    i += 1
-    if i > maxFrames:
+    img = np.zeros((height,width,3), np.uint8)
+    hPixels = width / int(blocks)
+    vPixels = height / int(blocks)
+    # print vBlocks,hBlocks
+    # imgCells = (blockPx) * (rows * cols)
+
+    # vertical blocks
+    for i in range(totalBlocks):
+      yPos = i * vPixels
+      yMax = yPos + vPixels
+      for j in tqdm(range(totalBlocks)):
+        xPos = j * hPixels
+        xMax = xPos +hPixels
+        # print yPos,yMax,xPos,xMax
+        cellCount = 0
+        rTot = 0
+        gTot = 0
+        bTot = 0
+        for ii in range(yPos,yMax):
+          for jj in range(xPos,xMax):
+            cellCount += 1
+            rTot += data[ii][jj][2]
+            gTot += data[ii][jj][1]
+            bTot += data[ii][jj][0]
+        rAvg = rTot / cellCount
+        gAvg = gTot / cellCount
+        bAvg = bTot / cellCount
+
+        cv2.rectangle(img, (xPos,yPos), (xPos+hPixels,yPos+vPixels), (bAvg,gAvg,rAvg), -1)
+    cv2.imwrite(destination+str(frame)+'_image.png',img)
+
+    frame += 1
+    if frame > maxFrames:
       break
 
 parser = argparse.ArgumentParser(description='f')
